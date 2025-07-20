081f84a1fbdd8c4792a6b0c7b681f1b3b65228ed
Ronen Friedman <rfriedma@redhat.com>
2025-07-20 16:18:07 +0300

    fixes
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- mode.cc
+++ mode.cc
@@ -25,7 +25,27 @@ void collect(mclct_t& mc, const src_t& dt_in)
    }
 }
 
-int main()
+
+void list_res(const mclct_t::res_type_t& res, string nm)
+{
+   int tag = static_cast<int>(res.tag);
+   const string nm_tag = [](int t) {
+       switch (t) {
+           case static_cast<int>(mclct_t::mode_status_t::no_mode_value):
+               return "..no mode.....";
+           case static_cast<int>(mclct_t::mode_status_t::mode_value):
+               return "..mode_value..";
+           case static_cast<int>(mclct_t::mode_status_t::authorative_value):
+               return "..autho.......";
+           default:
+               return "unknown";
+       }
+   }(tag);
+   cout << fmt::format("{}: mode result: tag:{},\tkey:{}, id:{}, count:{}\n",
+                       nm, nm_tag, res.key, res.id, res.count);
+}
+
+void basic_1st_test()
 {
    mclct_t mc{7, 5};
    src_t data = {
@@ -45,10 +65,43 @@ int main()
    cout << "Mode found: key = " << mode.key
         << ", id = " << mode.id
         << ", count = " << mode.count << endl;
-
-   return 0;
-
-
 }
 
+struct test_instance_t {
+  string nm_;
+  src_t dt_;
+  mclct_t::res_type_t expected_;
+};
+
+void run_test(const test_instance_t& test)
+{
+  mclct_t mc{test.dt_.size(), test.dt_.size()};
+  collect(mc, test.dt_);
+
+  auto result = mc.find_mode();
+  list_res(result, test.nm_);
+  assert(result.tag == test.expected_.tag);
+  if (result.tag != mclct_t::mode_status_t::no_mode_value) {
+    assert(result.key == test.expected_.key);
+    assert(result.id == test.expected_.id);
+    assert(result.count == test.expected_.count);
+  }
+}
+
+test_instance_t test_cases[] = {
+  {"Test 1", {{10, 100}, {11, 200}, {12, 150}, {13, 100}, {14, 100}},
+   {mclct_t::mode_status_t::authorative_value, 100, 14, 3}},
+  {"Test 1", {{10, 100}, {31, 200}, {32, 232}, {33, 233}, {34, 100}},
+   {mclct_t::mode_status_t::mode_value, 100, 34, 2}},
+  {"Test 2", {{20, 300}, {21, 300}, {22, 400}, {23, 500}, {24, 500}},
+   {mclct_t::mode_status_t::no_mode_value, 300, 20, 2}}
+};
+
+int main()
+{
+   basic_1st_test();
+   for (const auto& test : test_cases) {
+       run_test(test);
+   }
+}
 
--- mode.h
+++ mode.h
@@ -3,11 +3,14 @@
 // mode.h
 // This header file defines the mode_collector class template for collecting modes from a population of data.
 
-#include <cstddef>
-#include <functional>     // for std::identity
-#include <unordered_map>  // for std::unordered_map
-#include <ranges>
+#include <fmt/format.h>
+
 #include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <functional>  // for std::identity
+#include <ranges>
+#include <unordered_map>  // for std::unordered_map
 
 
 // The inputs are
@@ -23,10 +26,13 @@ class mode_collector {
 
   const size_t m_population_size;
   const size_t m_sample_size;  // i.e. the good candidates
-  std::unordered_map<K, node_type_t, std::identity>
-      m_frequency_map;  // Map to store frequency of each value
-  int actual_count{
-      0};  // Actual count of elements added. Must match m_sample_size.
+
+  /// \todo: consider PMR on the stack
+  /// Map to store frequency of each value
+  std::unordered_map<K, node_type_t, std::identity> m_frequency_map;
+
+  /// Actual count of elements added. Must match m_sample_size.
+  size_t actual_count{0};
 
 
  public:
@@ -44,7 +50,6 @@ class mode_collector {
     size_t count;
   };
 
-
   explicit mode_collector(size_t population_size, size_t sample_size)
       : m_population_size(population_size)
       , m_sample_size(sample_size)
@@ -57,12 +62,12 @@ class mode_collector {
   {
     auto& node = m_frequency_map[value];
     node.m_count++;
+    // note: it's OK to overwrite the ID here:
     node.m_id = obj;  // Store the object ID associated with this value
     actual_count++;
   }
 
   /// Get the mode of the collected values
-  /// \ATTN: destructive!
   res_type_t find_mode()
   {
     assert(actual_count == m_sample_size);
@@ -73,8 +78,6 @@ class mode_collector {
         m_frequency_map, {},
         [](const auto& pair) { return pair.second.m_count; });
 
-    //res_type_t result{mode_status_t mode_value, max_elem->first, max_elem->second.m_id, max_elem->second.m_count};
-
     // Check for clear victory
     if (max_elem->second.m_count > m_sample_size / 2) {
       return {
@@ -82,23 +85,78 @@ class mode_collector {
           max_elem->second.m_id, max_elem->second.m_count};
     }
 
-    // Check for possible ties (destructive, so prevent a 2nd call)
-    actual_count = 0;
+    // Check for possible ties
+    const auto max_elem_cnt = max_elem->second.m_count;
 
     max_elem->second.m_count = 0;  // Reset the count of the max element
     const auto second_best_elem = std::ranges::max_element(
         m_frequency_map, {},
         [](const auto& pair) { return pair.second.m_count; });
+    max_elem->second.m_count = max_elem_cnt;  // Restore the count
 
-    if (second_best_elem->second.m_count == max_elem->second.m_count) {
+    if (second_best_elem->second.m_count == max_elem_cnt) {
       return {
           mode_status_t::no_mode_value, max_elem->first, max_elem->second.m_id,
-          max_elem->second.m_count};
+          max_elem_cnt};
     }
 
     return {
         mode_status_t::mode_value, max_elem->first, max_elem->second.m_id,
-        max_elem->second.m_count};
+        max_elem_cnt};
   }
 };
 
+//namespace fmt {
+
+// Custom formatter for mode_collector::res_type_t
+
+// template <>
+// struct fmt::formatter<typename mode_collector<int, uint64_t, 0>::res_type_t> {
+//   template <typename ParseContext>
+//   constexpr auto parse(ParseContext& ctx)
+//   {
+//     return ctx.begin();
+//   }
+// 
+//   using rest = mode_collector<int, uint64_t, 0>::res_type_t;
+// 
+//   template <typename FormatContext>
+//   auto format(const rest& res, FormatContext& ctx) const -> fmt::v10::appender
+//   {
+//     return format_to(
+//         ctx.out(), "Mode: {}, Key: {}, ID: {}, Count: {}",
+//         static_cast<int>(res.tag), res.key, res.id, res.count);
+//   }
+// };
+
+
+// template <>
+// struct formatter<Scrub::ScrubJob> {
+//   constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+//
+//   template <typename FormatContext>
+//   auto format(const Scrub::ScrubJob& sjob, FormatContext& ctx) const
+//   {
+//     return fmt::format_to(
+// 	ctx.out(), "pg[{}]:sh:{}/dp:{}<{}>",
+// 	sjob.pgid, sjob.shallow_target, sjob.deep_target, sjob.state_desc());
+//   }
+// };
+
+// template <typename OBJ_ID, typename K>
+// struct fmt::formatter<typename mode_collector<OBJ_ID, K, 0>::res_type_t> {
+//   template <typename ParseContext>
+//   constexpr auto parse(ParseContext& ctx) {
+//     return ctx.begin();
+//   }
+//
+//   using rest = mode_collector<OBJ_ID, K, 0>::res_type_t;
+//
+//   template <typename FormatContext>
+//   auto format(const rest& res,
+//               FormatContext& ctx) const {
+//     return format_to(ctx.out(), "Mode: {}, Key: {}, ID: {}, Count: {}",
+//                      static_cast<int>(res.tag), res.key, res.id, res.count);
+//   }
+// };
+//} // namespace fmt
